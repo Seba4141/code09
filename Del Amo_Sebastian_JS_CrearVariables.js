eed1115a183065207c493ce839176c3e3b172c3c
Sebastian Del Amo <sebastiandelamo41@gmail.com>
2022-09-27 16:04:13 -0300

    first commit

---
+++ Del Amo_Sebastian_JS_CrearVariables.js
@@ -0,0 +1,4 @@
+var edadmin = 10
+var alturametromin = 1
+// se deberia usar las varientes como limite, ya que todo el que
+// pase debera cumplir con las condiciones de ser mayores a ambas variantes 
